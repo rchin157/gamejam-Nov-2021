2363a84de31e3c19747081c9096fb2884643a68a
Xulxuvar <teridax1306@gmail.com>
2021-11-07 16:00:06 -0700

    Finished Screen is more kino
    ^

--- scripts/finishGame.gd
+++ scripts/finishGame.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+var particles
 var onText = false
+var recipe
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Sfx.stopSongs()
+	recipe = get_node("CanvasLayer/Recipe")
+	particles = [get_node("CanvasLayer/Particles2D"),get_node("CanvasLayer/Particles2D2")]
 	Sfx.enableSong(4)
 	if Items.win:
 		$CanvasLayer/AnimatedSprite.set_animation("Win")
@@ -17,7 +21,24 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func generateRecipe():
+	var winText = "The Winning Recipe Was:\n"
+	for i in range(Items.lastStep):
+		winText = winText+Items.usedNames[Items.allVItems[i].ID]+",\n\n"
+	winText = winText+"and finally...\n A Dash of Fulminate"
+	print(Items.lastStep)
+	recipe.set_text(winText)
+	recipe.show()
 
+func toggleParticles(toggle):
+	if toggle:
+		particles[0].show()
+		particles[1].show()
+		generateRecipe()
+	else:
+		particles[0].hide()
+		particles[1].hide()
+		recipe.hide()
 func _on_AnimatedSprite_animation_finished():
 	$CanvasLayer/Button.show()
 
@@ -27,6 +48,7 @@ func _on_Button_pressed():
 		onText = true
 		$CanvasLayer/ColorRect.show()
 		if Items.win:
+			toggleParticles(false)
 			$CanvasLayer/ColorRect/wintext.show()
 		else:
 			$CanvasLayer/ColorRect/losetext.show()
@@ -38,6 +60,7 @@ func _on_AnimatedSprite_frame_changed():
 	if Items.win:
 		if $CanvasLayer/AnimatedSprite.frame == 2:
 			Sfx.stopSongs()
+			toggleParticles(true)
 			Sfx.enableSong(2)
 			$CanvasLayer/AnimatedSprite.stop()
 			$CanvasLayer/Button.show()
